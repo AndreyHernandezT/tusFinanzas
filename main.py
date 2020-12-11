0066295bc4da9c42500af3439555976e880bfc01
Andrey Hernandez <andreyhernandeztorres@gmail.com>
2020-12-11 00:43:59 -0500

    second commit

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-from db.user_db import UserInDB
-from db.user_db import update_user, get_user
-from db.transaction_db import TransactionInDB
-from db.transaction_db import save_transaction
-from models.user_models import UserIn, UserOut
-from models.transaction_models import TransactionIn, TransactionOut
+from db.registro_usuario_db import usuarioDB
+from db.registro_usuario_db import update_usuario, get_usuario
+from models.usuarios_models import InicioSesion, Register
 
 import datetime
 from fastapi import FastAPI, HTTPException
@@ -11,15 +8,27 @@ from fastapi import FastAPI, HTTPException
 api = FastAPI()
 
 @api.post("/user/auth/")
-async def auth_user(user_in: UserIn):
+async def auth_user(user_in: InicioSesion):
 
-    user_in_db = get_user(user_in.username)
+    nuevo_usuario = get_usuario(user_in.usuario)
 
-    if user_in_db == None:
+    if nuevo_usuario == None:
         raise HTTPException(status_code=404, detail="El usuario no existe")
 
-    if user_in_db.password != user_in.password:
+    if nuevo_usuario.contrasena != user_in.contrasena:
         return  {"Autenticado": False}
 
     return  {"Autenticado": True}
 
+
+@api.get("/user/usuarios/{usuario}")
+async def get_user(usuario: str):
+
+    nuevo_usuario = get_usuario(usuario)
+
+    if nuevo_usuario == None:
+        raise HTTPException(status_code=404, detail="El usuario no existe")
+
+    user_out = Register(**nuevo_usuario.dict())
+
+    return  user_out
